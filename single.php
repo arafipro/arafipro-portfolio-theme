e4da3953fa54d8094d8ab97fdfac2a97ebba5c71
Hiroki Chiyoda <contact@arafipro.com>
2024-03-02 16:45:24 +0900

    記事の個別ページを配置

--- single.php
+++ single.php
@@ -7,19 +7,7 @@
   <body>
 		<?php get_template_part("components/nav/header-nav") ?>
     <main>
-		<?php	if(have_posts()): while(have_posts()): the_post(); ?>
-			<h1>
-				<?php the_title() ?>あああああ
-			</h1>
-			<?php the_content() ?>
-			<ul>
-				<li>投稿日：<?php the_date('Y年m月d日') ?></li>
-				<li>更新日：<?php the_modified_date('Y年m月d日') ?></li>
-				<li>投稿者：<?php the_author() ?></li>
-				<li>カテゴリー：<?php the_category(",") ?></li>
-				<li>タグ：<?php the_tags() ?></li>
-			</ul>
-		<?php endwhile; endif; ?>
+			<?php get_template_part("components/single/discription") ?>
     </main>
 		<?php get_template_part("components/nav/footer-nav") ?>
 		<!-- footer.phpを読み込む -->
