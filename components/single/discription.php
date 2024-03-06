4e8d36b2bf55bd710362726f0c737a50493d9677
Hiroki Chiyoda <contact@arafipro.com>
2024-03-06 17:47:46 +0900

    記事投稿の表示を変更

--- components/single/discription.php
+++ components/single/discription.php
@@ -3,13 +3,11 @@
 		<h1 class="top-title">
 			<?php the_title() ?>
 		</h1>
-		<?php the_content() ?>
 		<div class="info">
 			<p>投稿日：<?php the_date('Y年m月d日') ?></p>
 			<p>更新日：<?php the_modified_date('Y年m月d日') ?></p>
-			<p>投稿者：<?php the_author() ?></p>
-			<p>カテゴリー：<?php the_category(",") ?></p>
-			<p>タグ：<?php the_tags() ?></p>
 		</div>
+		<p class="author">投稿者：<?php the_author() ?></p>
+		<?php the_content() ?>
 	<?php endwhile; endif; ?>
 </section>
